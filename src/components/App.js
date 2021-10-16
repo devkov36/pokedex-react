ae4f9002c6f1abb8a5ce6cfb4769f56dfa0dbfc4
Maria Fernanda <mafer13cp@gmail.com>
2021-10-15 21:37:52 -0600

    Componente de title agregado con su hoja de estilo

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hola Team :v</h1>
+        <h1>Hola Team >:c</h1>
       </header>
     </div>
   );
