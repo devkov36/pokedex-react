f1c8ebfccf07b92a056e947aa6489fb91ca54572
Maria Fernanda <mafer13cp@gmail.com>
2021-10-17 00:26:54 -0600

    PokemonImage componente y css agregados, PokemonType componente y css actualizados con colores por tipo

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Title from "./pokemon/Title";
 import PokemonType from "./pokemon/PokemonType";
+import PokemonImage from "./pokemon/PokemonImage";
+const typesP = ["grass", "poison"];
 function App() {
   return (
     <div className="App">
@@ -10,7 +12,8 @@ function App() {
           img="https://cdn2.bulbagarden.net/upload/thumb/b/b5/Flying_icon_SwSh.png/96px-Flying_icon_SwSh.png"
         />
       </header>
-      <PokemonType type="flying"/>
+      <PokemonType type="grass"/>
+      <PokemonImage types= {typesP} src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"/>
     </div>
   );
 }
