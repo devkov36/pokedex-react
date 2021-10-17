84678aacdc4bce9fec1e735f900587c7b4e859e2
EnriqueHM <enriqhz@gmail.com>
2021-10-17 12:06:38 -0500

    Se agregan versiones minimas de componentes PokeHeader y PokeBall asi como sus estilos basicos

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
+
+import PokeHeader from './header/PokeHeader';
+
 function App() {
   return (
+
     <div className="App">
       <header className="App-header">
         <h1>Hola Team >:c</h1>
       </header>
+      <PokeHeader />      
     </div>
+
   );
 }
 
