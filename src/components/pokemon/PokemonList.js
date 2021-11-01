cc885299b525b0e99ec7dc639189ed6b0c1e3e13
ernestov <er.velasco94@gmail.com>
2021-11-01 12:17:28 -0600

    Se limpia codigo en Pokemon List y se agregan breakpoints

--- src/components/pokemon/PokemonList.js
+++ src/components/pokemon/PokemonList.js
@@ -9,38 +9,42 @@ function PokemonList(props) {
   const orderedPokemons = pokemons.sort((a, b) => {
     return a.pokedexNumber - b.pokedexNumber;
   });
-  const classificationList = orderedPokemons.map(
-    (pokemon) => pokemon.classification
-  );
-  const classificationFilter = classificationList.filter(
-    (classification, index, self) => self.indexOf(classification) === index
-  );
   const isPokemonListEmpty = pokemons.length === 0;
   return (
     <div className="pokemon-list">
       <header className="pokemon-list-header">
         {!isPokemonListEmpty ? (
-          <Title
-            list={classificationFilter}
-            page={props.page}
-            title={props.title}
-            img={props.imgHeader}
-          />
-        ) : undefined}
+          <Title page={props.page} title={props.title} img={props.imgHeader} />
+        ) : (
+          <Title page={props.page} title={props.title} />
+        )}
       </header>
       <section>
-        <Grid container spacing={2}>
-          {orderedPokemons.map((pokemon, index) => (
-            <Grid key={index} item xs={3}>
-              <PokemonCard
-                imageUrl={pokemon.imageUrl}
-                pokedexNumber={pokemon.pokedexNumber}
-                name={pokemon.name}
-                types={pokemon.types}
-              />
-            </Grid>
-          ))}
-        </Grid>
+        {!isPokemonListEmpty ? (
+          <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          >
+            {orderedPokemons.map((pokemon, index) => (
+              <Grid key={index} item xs={12} sm={6} md={3}>
+                <PokemonCard
+                  imageUrl={pokemon.imageUrl}
+                  pokedexNumber={pokemon.pokedexNumber}
+                  name={pokemon.name}
+                  types={pokemon.types}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        ) : (
+          <div className="pokemon-list-empty">
+            <h2>We're sorry! We couldn't find results for "{props.title}".</h2>
+            <h3>
+              Please try with a pokedex number between 1 and 151 or a letter.
+            </h3>
+          </div>
+        )}
       </section>
     </div>
   );
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import SearchBtn from "./SearchBtn";
 import { capitalizeStrings } from "../../utils/capitalizeStrings";
-
 import "../../css/search/search.css";
 
 function Search() {
@@ -16,7 +15,7 @@ function Search() {
         placeholder="Search by name or Pokedex number"
         onFocus={(e) => (e.target.value = "")}
         onChange={(e) =>
-          setSearch(e.target.value ? e.target.value : "No results")
+          setSearch(e.target.value ? e.target.value : "Empty search")
         }
       />
       <SearchBtn search={formatSearch} />
