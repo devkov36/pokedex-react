76abbd2a200cca9d4615451c3da2a981d50f711a
Maria Fernanda <mafer13cp@gmail.com>
2021-11-04 01:26:37 -0700

    Colores de fondo de imagen depokemon actualizados

--- src/components/pokemon/PokemonImage.js
+++ src/components/pokemon/PokemonImage.js
@@ -2,26 +2,10 @@ import React from "react";
 import "../../css/pokemon/pokemonImage.css";
 class PokemonImage extends React.Component {
   render() {
-        //const r = document.querySelector(':root');
-      if (this.props.types.length === 2)
-      {
-        //r.style.setProperty('--color1', typeColors[this.props.types[0]]);
-        //r.style.setProperty('--color2', typeColors[this.props.types[1]]);
-          return (
-            <div className={`typeBackground ${this.props.types[0]}-back`}>
-                <div className={`typeBackground2 ${this.props.types[1]}-back2`}>
-                    <img className="pokemonImg" src={this.props.src} alt="Pokemon"/>
-                </div>
-            </div>
-          );
-      }
-      //r.style.setProperty('--color2', typeColors[this.props.types[0]]);
       return (
-        <div className={`typeBackground`}>
-            <div className={`typeBackground2 ${this.props.types[0]}-back2`}>
-                <img className="pokemonImg" src={this.props.src} alt="Pokemon"/>
-            </div>
-        </div>
+          <div className={`typeBackground ${this.props.types[0]}-back`}>
+              <img className="pokemonImg" src={this.props.src} alt="Pokemon"/>
+          </div>
       );
   }
 }
