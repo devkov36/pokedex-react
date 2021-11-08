dcc0d9b4421ec4fe6934852648ef819bab5bcfe4
EnriqueHM <enriqhz@gmail.com>
2021-11-07 22:56:24 -0600

    Se agrega link a home en la pokebola

--- src/components/header/Pokeball.js
+++ src/components/header/Pokeball.js
@@ -1,20 +1,23 @@
 import React  from "react";
 import '../../css/header/pokeball.css'
-import pokeball from '../../assets//pokeball.png'  //'./pokeball.png'
+import pokeball from '../../assets//pokeball.png'
 import Search from "../search/Search";
+import {Link} from 'react-router-dom';
 
-// TODO: Agregar boton de pokebola
-// TODO: Agregar enlace o accion que hara al recibir un click
+
 function PokeBall(){
-    return (
+    return (        
         <section id="ext-container">
-            <section id="in-container">
-                <img src={pokeball} alt="Pokeball" id="pokeButton" />
+            <section >
+                <Link to='/' id="in-container">
+                <img src={pokeball} alt="Pokeball" id="pokeButton" onClick={GoHome} />
                 <p id="pageTitle">Pokedex</p>
+                </Link>
             </section>
             <Search className="search"/>   
         </section>
     );
 }
+
 
 export default PokeBall;
