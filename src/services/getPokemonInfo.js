e79280581fad1850bcd1a7099afe7df12563aad0
Bryan Moreno <abre.go@outlook.com>
2021-11-07 23:00:17 -0600

    get all class

--- src/services/getPokemonInfo.js
+++ src/services/getPokemonInfo.js
@@ -1,5 +1,6 @@
 const URL = "https://pokemon-bedu.herokuapp.com/v1/pokemons";
 const URLtype = "https://pokemon-bedu.herokuapp.com/v1/types/";
+const URLclass = "https://pokemon-bedu.herokuapp.com/v1/classification/";
 
 export const getAllPokemons = async () => {
   const response = await fetch(URL);
@@ -15,6 +16,12 @@ export const getSinglePokemon = async (id) => {
 
 export const getAllTypes = async (id) => {
   const response = await fetch(URLtype + id);
+  const dataType = await response.json();
+  return dataType;
+};
+
+export const getAllClassifications = async () => {
+  const response = await fetch(URLclass);
   const dataType = await response.json();
   return dataType;
 };
